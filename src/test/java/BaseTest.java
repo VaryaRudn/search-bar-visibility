eaaa808107677e5f1c41e60249c8ba075e884c9f
Varya <vararudneva@gmail.com>
2025-05-12 15:14:55 +0300

    BaseTest

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,14 +1,23 @@
+import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
-import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 
-public class BaseTest {
-    @BeforeEach
+
+abstract public class BaseTest {
+
     public void setUp() {
         // Открываем нужный сайт
+
+        Configuration.browser = "chrome";
+        Configuration.browserSize = "1920x1080";
+        Configuration.headless = true;
         Selenide.open("https://severstal.com/rus/");
+    }
+
+    @BeforeEach
+    public void init(){
+        setUp();
     }
 
     @AfterEach
